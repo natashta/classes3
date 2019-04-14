0da88af10d522ae217418538a569110e72219059
natashta <natashta@local>
2019-04-14 18:04:10 +0300

    lint fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,7 @@
 const getNumber = (str) => {
   try {
-      if(/^[1-9]\d*$/.test(str) && str > 0)
-        return Number(str);
-    else throw new Error('Ввод некорректен');
+    if (/^[1-9]\d*$/.test(str) && str > 0) return Number(str);
+    throw new Error('Ввод некорректен');
   } catch (e) {
     return e;
   }
